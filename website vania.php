c0371c385ce5d2af24be21c5436d4d1dbdf19ea8
kirbvy <128491585+kirbvy@users.noreply.github.com>
2023-03-22 05:42:21 +0700

    Add files via upload

---
+++ website vania.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <head>
+      <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <!--- mohon maaf pak saya salah info ternyata diperlukan JS juga, jadi saya mengumpulkan ulang dengan sisipan code JS sedikit, terima kasih, pak.--->
+
+
+  </head>
+  <body class="antialiased">
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My Resume</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      </head>
+      <body>
+      <p id="time"></p>
+      <script>
+          document.getElementById("time").innerHTML = "Vania 2SIMA 2231038";
+      </script> 
+
+        <div class="wraper">
+          <div class="container">
+            <div class="left-content p-30">
+              <div class="logo">
+                <a href="index.html">
+                  <span>My</span>
+                  <span>Resume</span>
+                </a>
+              </div>
+              <div class="nav">
+                <ul>
+                  <li><a href="#Home">Home</a></li>
+                  <li><a href="#About">About</a></li>
+                  <li><a href="">Work Profile</a></li>
+                  <li><a href="">Portfolio</a></li>
+                  <li><a href="">Education</a></li>
+                  <li><a href="">Contact</a></li>
+                </ul>
+              </div>
+              <div class="left-footer">
+                <div class="social-icons">
+                  <ul>
+                    <li><a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                  </ul>
+                </div>
+                <div class="foot-contact">
+                  <ul>
+                    <li>videllavania11@gmail.com</li>
+                    <li>(+62)82288881114</li>
+                  </ul>
+                </div>
+
+                <div class="copy-rights">Copyright &copy;2019 All rights reserved</div>
+              </div>
+
+            </div>
+            <div class="main-content">
+              <div class="section1 p-30" id="Home">
+                <div class="sec-title">
+                  <div class="pg-sub-title">Home</div>
+                  <div class="pg-title">Profile</div>
+                </div>
+                <div class="d-flex-align-center">
+                  <div class="user-img flex-1"><img src="https://i.pinimg.com/736x/e6/32/70/e63270db91a0921965e32d50ae2b9b40.jpg"></div>
+                  <div class="user-profile flex-1">
+                    <div class="pro-details">
+                      <div class="sm-text"> my name is</div>
+                      <div class="name">Vania</div>
+                      <div class="role">College Student</div>
+
+                    </div>
+                    <div class="text">Graduated from a private catholic high school in Riau, East Sumatra. Previously
+                    worked in the creative design and academic field independently as a
+                      student.
+                      Able to demonstrate a good communication skill through various
+                      activities in high school and social/volunteer activities.</div>
+                    <div class="pr-list">
+                      <ul>
+                        <li><i class="fa fa-calendar" aria-hidden="true"></i> 3rd of November 2004</li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i> (+62)82288881114</li>
+                        <li><i class="fa fa-envelope" aria-hidden="true"></i> videllavania11@gmail.com</li>
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> Batam, Indonesia</li>
+                      </ul>
+                    </div>
+                    <div class="social-icons-head">
+                      <ul>
+                        <li><a href=""><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                        <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                      </ul>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+          
+
+          
+
+      </body>
+    </html>
+  </body>
+</html>
